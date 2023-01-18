dc4488467fdf51bee832595c2e7d80f655cc3049
FacuHTz <hetzefacundo@gmail.com>
2023-01-18 19:50:15 -0300

    promises

---
+++ src/es6/07-promises.js
@@ -0,0 +1,13 @@
+//Una promesa es una forma de manejar el asincronismo en JavaScript y se representa como un objeto que puede generar un valor único a futuro, que tiene dos estados, o está resuelta o incluye una razón por la cual no ha sido resuelta la solicitud. 
+const anotherFunction= () => {
+    return new Promise((resolve, reject) => {
+        if (true){
+            resolve("si");
+        }else{
+            reject("no");
+        }
+    })   
+}
+
+anotherFunction()
+.then(response => console.log(response)).catch(err => console.log(err));
