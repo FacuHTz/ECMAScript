66fd77356581a1e7ce167a30d2459071d35c9bb9
FacuHTz <hetzefacundo@gmail.com>
2023-01-18 16:20:03 -0300

    arrow functions

---
+++ src/es6/01-arrow-function.js
@@ -0,0 +1,9 @@
+function square(num) {
+  return num * num;
+}
+
+const square = (num) => {
+  return num * num;
+};
+
+const square = (num) => num * num; //una de las cosas que facilita la arrow function es que cuando una fuincion es simple no se necesita poner {} ni return y se puede hacer mas coomprimido
