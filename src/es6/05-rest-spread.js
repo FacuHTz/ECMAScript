46da82d93ac757333b33e274847aa3d248345b48
FacuHTz <hetzefacundo@gmail.com>
2023-01-18 19:39:01 -0300

    object literals

--- src/es6/05-rest-spread.js
+++ src/es6/05-rest-spread.js
@@ -1,11 +1,29 @@
 //array destructuring
 
-let fruits = ["apple", "banana"]
-let[a, b] = fruits;
+let fruits = ["apple", "banana"];
+let [a, b] = fruits;
 console.log(a, fruits[1]);
 
 //object destructuring
 
-let user = {username:"Oscar", age: 34};
-let {username, age} = user;
+let user = { username: "Oscar", age: 34 };
+let { username, age } = user;
 console.log(username, user.age);
+
+//spread operator
+
+let person = { name: "Oscar", age: 24 };
+let country = "MX";
+
+let data = { id: 1, ...person, country };
+console.log(data);
+
+//rest
+
+function sum(num, ...values) {
+  console.log(values);
+  console.log(num + values[0]);
+  return num + values[0];
+}
+
+sum(1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
